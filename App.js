166b725254db88eced205ed90533728177b3f728
bhagatsingh1998 <bhagatsingh.17is@saividya.ac.in>
2020-12-10 10:42:24 +0530

    5. Working with Core Components

--- App.js
+++ App.js
@@ -4,18 +4,28 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app. </Text>  
+    // </View>
+
+    // redesigning the view according to our appp design
+    <View>
+      <View>
+        {/* Inout box and add button */}
+      </View>
+      <View>
+        {/* List of goals */}
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// not required
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
