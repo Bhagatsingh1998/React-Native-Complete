f33c658bc53bf29663b300d64b29b42ab31fe6a4
bhagatsingh1998 <bhagatsingh.17is@saividya.ac.in>
2020-12-10 12:12:03 +0530

    9. Inline Styles & StyleSheet Objects

--- App.js
+++ App.js
@@ -1,23 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
+// we are getting our desired design but by using inline style which is not good
 export default function App() {
   return (
-    <View style={{padding: 35}}>
-      {/* inputbox and button shuould be side by side. RN uses flexbox */}
-      {/* flexbox postion elements side by side or below one another. */}
-      {/* justifycontent: align items to main axis. as flex direction is row, thus main axis is from L -> R. space-between: gives all empty space inbetween */}
-      {/* cross axis: opposit of main axis. alignItem aligns across cross-axis */}
-      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
-        {/* can also set width in pixels */}
-        <TextInput placeholder="Course Goal" style={{borderBottomColor: 'black', borderBottomWidth: 1, marginBottom: 3, padding: 10, width: '80%'}} /> 
-        <Button title="ADD" /> 
+    <View style={styles.sreen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Course Goal" style={styles.input} />
+        <Button title="ADD" />
       </View>
-      <View>
-        {/* List of goals */}
-      </View>
+      <View>{/* List of goals */}</View>
     </View>
   );
 }
- 
+
+// thus, using a StyleSheet object provided by RN.
+// we can even craete our own js object for style but StyleSheet object also do performance optimization and it also dectects incorrect styling
+
+const styles = StyleSheet.create({
+  // creating nested objcts for style
+  sreen: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  input: {
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+    marginBottom: 3,
+    padding: 10,
+    width: "80%",
+  },
+});
