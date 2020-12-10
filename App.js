333b885b3dc9756a69230918a53c88d190fbb4ac
bhagatsingh1998 <bhagatsingh.17is@saividya.ac.in>
2020-12-10 11:29:17 +0530

    6. Getting Started with Styles

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    // <View style={styles.container}>
-    //   <Text>Open up App.js to start working on your app. </Text>  
-    // </View>
-
-    // redesigning the view according to our appp design
-    <View>
+    // giving some padding
+    <View style={{padding: 35}}>
       <View>
-        {/* Inout box and add button */}
+        {/* 'black' is string here but color shortcuts are present which we can import */}
+        {/* 'border-bottom-color' will work in css and js but will not work in RN. thus, have to follow perticular pattern. */}
+        <TextInput placeholder="Course Goal" style={{borderBottomColor: 'black', borderBottomWidth: 1, marginBottom: 3, padding: 10}} /> 
+        <Button title="ADD" /> 
+        {/* We can see ADD btn but not the inputbox.that is because inputbox is hidden as stausbar of mobile is overlaping it. Thus, using style to give some padding */}
       </View>
       <View>
         {/* List of goals */}
@@ -19,13 +19,4 @@ export default function App() {
     </View>
   );
 }
-
-// not required
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
+ 
