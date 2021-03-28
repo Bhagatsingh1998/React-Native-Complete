743f15240b6c7e33e46596694795131bead1b003
Bhagat Singh <bhagatsingh.17is@saividya.ac.in>
2021-03-28 09:54:40 +0530

    2.17. Splitting the App Into Components

--- App.js
+++ App.js
@@ -9,6 +9,9 @@ import {
   ScrollView,
   FlatList,
 } from "react-native";
+// 
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
 
 export default function App() {
   const [courseGoals, setCourseGols] = React.useState([]);
@@ -41,39 +44,20 @@ export default function App() {
         <Button title="ADD" onPress={addGoalHandler} />
       </View>
       <View>
-        {/* ScrollView is good when number of items are limited in a list as it will render all the data which will be not seen on the screen. Thus scrollview is inefficinet for very long lists */}
-        {/* for long list, FlatList component i used. Flat list takes 2 imp args. data: takes a array as data. renderItem: its a function which iterates over the data array one by one and render it. */}
-        {/* in this, we dont have to pass key, as it expects staructured ata which will have key value in it */}
-        {/* to access the value, '.item' is used on itemData  */}
-        {/* <FlatList
-          data={courseGoals}
-          renderItem={(itemData) => (
-            <View style={styles.listItem}>
-              <Text>{itemData.item}</Text>
-            </View>
-          )}
-        />  */}
-
-
-        {/*our above array data is not strutctured ie, its not haveing any key value. Rearraging our data in array */}
-        {/* <FlatList
-          data={courseGoals}
-          renderItem={(itemData) => (
-            <View style={styles.listItem}>
-              <Text>{itemData.item.value}</Text>
-            </View>
-          )}
-        />  */}
-
-        {/* it maky happen that we dont use 'key' value in array, but instaed of it rename 'key' to 'uid'. at that time we have to tell Flatlist to take 'uid' value as key */}
-        {/* FlatList has method ketExtrator which which extrats key name by default from an array of data */}
         <FlatList
           keyExtractor={(item, index) => item.uid}
           data={courseGoals}
           renderItem={(itemData) => (
-            <View style={styles.listItem}>
-              <Text>{itemData.item.value}</Text>
-            </View>
+            //
+            // <View style={styles.listItem}>
+            //   <Text>{itemData.item.value}</Text>
+            // </View>
+
+            // 
+            // <GoalItem>{itemData.item.value}</GoalItem>
+
+            <GoalItem title={itemData.item.value} />
+
           )}
         /> 
       </View>
@@ -97,11 +81,11 @@ const styles = StyleSheet.create({
     padding: 10,
     width: "80%",
   },
-  listItem: {
-    padding: 10,
-    backgroundColor: `#ccc`,
-    borderColor: "black",
-    borderWidth: 1,
-    marginVertical: 10,
-  },
+  // listItem: {
+  //   padding: 10,
+  //   backgroundColor: `#ccc`,
+  //   borderColor: "black",
+  //   borderWidth: 1,
+  //   marginVertical: 10,
+  // },
 });
---
+++ components/GoalInput.js
@@ -0,0 +1,7 @@
+import react from 'react';
+
+const GoalInput = props => {
+  return <div></div>
+}
+
+export default GoalInput;
