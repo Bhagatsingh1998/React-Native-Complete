91244e7c8f28a3703c3b5eaebf17681516ca65d9
bhagat singh <bhagatsinghsinghsingh1998@gmail.com>
2021-04-05 09:42:46 +0530

    4.5. Working on the Layout

--- screens/StartGameSceen.js
+++ screens/StartGameSceen.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, TextInput, Button} from 'react-native';
 
 export default function StartGameSceen() {
   return (
     <View style={styles.screen}>
-      <Text>Start Game Screen</Text>
+      
+      <Text style={styles.title}>Start Game Screen</Text>
+      {/* we want to have input field and buttons. and make it feel like these components are siting on card. */}
+
+      <View style={styles.inputContainer}>
+        <Text>Select a Number</Text>
+        <TextInput />
+        {/* now 2 buttons, siting next to each other , thus another view comp */}
+        <View style={styles.btnContainer}>
+          <Button title="Reset" onPress={() => {}} />
+          <Button title="Confirm" onPress={() => {}} />
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +25,21 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     padding: 10,
-    // here alignItems will align the itemsfrom left to right as 'View' bydefault uses flexbox an flexbox by default uses flexbox direction coloumn.
     alignItems: 'center',
-
-  }
+  },
+  title : {
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  inputContainer: {
+    width: 300,
+    maxWidth: '80%',
+    alignItems: 'center',
+  },
+  btnContainer: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-around', 
+    padding: 15
+  },
 })
