7ee08f4fbd155555f782b685f8a7eba6b5380b67
bhagat singh <bhagatsinghsinghsingh1998@gmail.com>
2021-04-05 10:12:52 +0530

    4.7. React Native Styling vs CSS Styling

--- screens/StartGameSceen.js
+++ screens/StartGameSceen.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import {View, Text, StyleSheet, TextInput, Button} from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, TextInput, Button } from "react-native";
 
 export default function StartGameSceen() {
   return (
     <View style={styles.screen}>
-      
       <Text style={styles.title}>Start Game Screen</Text>
-      {/* we want to have input field and buttons. and make it feel like these components are siting on card. */}
-
       <View style={styles.inputContainer}>
         <Text>Select a Number</Text>
         <TextInput />
-        {/* now 2 buttons, siting next to each other , thus another view comp */}
         <View style={styles.btnContainer}>
           <Button title="Reset" onPress={() => {}} />
           <Button title="Confirm" onPress={() => {}} />
@@ -25,21 +21,40 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     padding: 10,
-    alignItems: 'center',
+    alignItems: "center",
   },
-  title : {
+  title: {
     fontSize: 20,
     marginVertical: 10,
   },
   inputContainer: {
     width: 300,
-    maxWidth: '80%',
-    alignItems: 'center',
+    maxWidth: "80%",
+    alignItems: "center",
+
+
+    // shadow property ONLY apply on iOS.
+    // giving shadow : it takes 4 attributes : color, offset(takes an object, takes height and width), opacity(1: fully opaque), radius(how shapre the shawdow is)
+    shadowColor: 'black',
+    shadowOffset: {
+      width: 0, height: 2
+    },
+    shadowOpacity: 0.26,
+    shadowRadius: 6,
+    backgroundColor: 'white',
+
+    // to apply shadow on android, elevation property is used.
+    elevation : 15,
+    padding: 15,
+    borderRadius: 10
+  
   },
   btnContainer: {
-    flexDirection: 'row',
-    width: '100%',
-    justifyContent: 'space-around', 
-    padding: 15
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-around",
+    padding: 15,
   },
-})
+
+ 
+});
